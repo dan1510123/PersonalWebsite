6235cc491c2ada97bbaa3f5e284c6bd650884038
dan1510123 <dan1510123@gmail.com>
2020-06-07 13:17:05 -0400

    Added dropdown on hover for navigation bar and changed Privacy page to a Contact page

--- PersonalWebsite/wwwroot/js/site.js
+++ PersonalWebsite/wwwroot/js/site.js
@@ -1,4 +1,9 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your Javascript code.
+/** Open dropdown on hover */
+$('li.dropdown').hover(function () {
+    $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(300);
+}, function () {
+    $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(300);
+});
