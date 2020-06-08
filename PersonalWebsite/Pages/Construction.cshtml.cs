19e853aab73f596d40c026154a091c52ab1d73ed
dan1510123 <dan1510123@gmail.com>
2020-06-07 20:13:28 -0400

    Added construction page and bootstrap-override Added construction page that uses data-passing between pages. Also added a bootstrap-override css hashtag type so that the whole body will weigh anything with #bootstrap-override as more important.

---
+++ PersonalWebsite/Pages/Construction.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace PersonalWebsite.Pages
+{
+    public class ConstructionModel : PageModel
+    {
+        private readonly ILogger<ConstructionModel> _logger;
+        public string PageType;
+
+        public ConstructionModel(ILogger<ConstructionModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet(string pagetype)
+        {
+            PageType = pagetype;
+        }
+    }
+}
