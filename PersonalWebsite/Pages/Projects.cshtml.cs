cc39cf0ccc41a46a809e7c91d62f55fc12739c2a
dan1510123 <dan1510123@gmail.com>
2020-06-20 18:27:15 -0400

    Got links to pages working for Projects

---
+++ PersonalWebsite/Pages/Projects.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace PersonalWebsite.Pages
+{
+    public class ProjectsModel : PageModel
+    {
+        private readonly ILogger<AboutModel> _logger;
+
+        public ProjectsModel(ILogger<AboutModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
