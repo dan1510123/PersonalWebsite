a2b338897da1f2198c79d91f51fa1f92539a8d48
dan1510123 <dan1510123@gmail.com>
2020-06-20 19:30:36 -0400

    Made first table for All Projects

---
+++ PersonalWebsite/Pages/ProjectEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PersonalWebsite.Pages
+{
+    public class ProjectEntry
+    {
+        public string projectName;
+        public string description;
+        public string[] languages;
+        public string timePeriod;
+        public string link;
+        public ProjectEntry(string projectName, string description,
+            string[] languages, string timePeriod, string link)
+        {
+            this.projectName = projectName;
+            this.description = description;
+            this.languages = languages;
+            this.timePeriod = timePeriod;
+            this.link = link;
+        }
+    }
+}
--- PersonalWebsite/Pages/Projects.cshtml.cs
+++ PersonalWebsite/Pages/Projects.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,7 @@ namespace PersonalWebsite.Pages
     public class ProjectsModel : PageModel
     {
         private readonly ILogger<AboutModel> _logger;
+        public ProjectEntry[] projects = new ProjectEntry[3];
 
         public ProjectsModel(ILogger<AboutModel> logger)
         {
@@ -19,6 +21,21 @@ namespace PersonalWebsite.Pages
 
         public void OnGet()
         {
+            projects[0] = new ProjectEntry("Spotify Playlist Analyzer",
+                "Analyzes Spotify playlist attributes",
+                new string[] { "awk", "html", "css" },
+                "Spring 2020",
+                "www.spotify.com");
+            projects[1] = new ProjectEntry("Realm Tracker",
+                "Tracker for multiple players' stats in Realm Royale",
+                new string[] { "awk", "html", "css" },
+                "Spring 2020",
+                "www.realmtracker.com");
+            projects[2] = new ProjectEntry("Vetris",
+                "Game similar to Tetris using voice",
+                new string[] { "javascript", "html" },
+                "Spring 2020",
+                "www.tetris.com");
         }
     }
 }
