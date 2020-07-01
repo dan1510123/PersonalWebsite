fb31eed4a48d10e0eeed860d0a01952885ccdd94
dan1510123 <dan1510123@gmail.com>
2020-07-01 00:05:12 -0400

    Finished Experiences page

---
+++ PersonalWebsite/Pages/Experiences.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace PersonalWebsite.Pages
+{
+    public class ExperiencesModel : PageModel
+    {
+        private readonly ILogger<AboutModel> _logger;
+
+        public ExperiencesModel(ILogger<AboutModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
